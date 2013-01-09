4c566815994cd4bd1b61afc49aadb6e04ea3cc23
Johnny Xu <gaga1227@gmail.com>
2013-01-09 20:10:04 +1100

    added files for personal website
    moved latest files from extel hosting

---
+++ js/gallery.js
@@ -0,0 +1,219 @@
+jQuery(document).ready(function() {
+	/*****************************************************************************/
+	//WEB
+	/*****************************************************************************/
+	$("a[rel=HA]").fancybox({
+		'transitionIn'		: 'fade',
+		'transitionOut'		: 'fade',
+		'titlePosition' 	: 'outside',
+		'titleFormat'       : function(title, currentArray, currentIndex, currentOpts) {
+		    return '<span id="fancybox-title-over">'+(currentIndex + 1)+' / '+currentArray.length+'&nbsp;&nbsp;&nbsp;'+title+'</span>';
+		}
+	});
+	$("a[rel=TC]").fancybox({
+		'transitionIn'		: 'fade',
+		'transitionOut'		: 'fade',
+		'titlePosition' 	: 'outside',
+		'titleFormat'       : function(title, currentArray, currentIndex, currentOpts) {
+		    return '<span id="fancybox-title-over">'+(currentIndex + 1)+' / '+currentArray.length+'&nbsp;&nbsp;&nbsp;'+title+'</span>';
+		}
+	});
+	$("a[rel=FW]").fancybox({
+		'transitionIn'		: 'fade',
+		'transitionOut'		: 'fade',
+		'titlePosition' 	: 'outside',
+		'titleFormat'       : function(title, currentArray, currentIndex, currentOpts) {
+		    return '<span id="fancybox-title-over">'+(currentIndex + 1)+' / '+currentArray.length+'&nbsp;&nbsp;&nbsp;'+title+'</span>';
+		}
+	});
+	$("a[rel=HAG]").fancybox({
+		'transitionIn'		: 'fade',
+		'transitionOut'		: 'fade',
+		'titlePosition' 	: 'outside',
+		'titleFormat'       : function(title, currentArray, currentIndex, currentOpts) {
+		    return '<span id="fancybox-title-over">'+(currentIndex + 1)+' / '+currentArray.length+'&nbsp;&nbsp;&nbsp;'+title+'</span>';
+		}
+	});
+	$("a[rel=HAG2]").fancybox({
+		'transitionIn'		: 'fade',
+		'transitionOut'		: 'fade',
+		'titlePosition' 	: 'outside',
+		'titleFormat'       : function(title, currentArray, currentIndex, currentOpts) {
+		    return '<span id="fancybox-title-over">'+(currentIndex + 1)+' / '+currentArray.length+'&nbsp;&nbsp;&nbsp;'+title+'</span>';
+		}
+	});
+	$("a[rel=LDH]").fancybox({
+		'transitionIn'		: 'fade',
+		'transitionOut'		: 'fade',
+		'titlePosition' 	: 'outside',
+		'titleFormat'       : function(title, currentArray, currentIndex, currentOpts) {
+		    return '<span id="fancybox-title-over">'+(currentIndex + 1)+' / '+currentArray.length+'&nbsp;&nbsp;&nbsp;'+title+'</span>';
+		}
+	});
+	$("a[rel=PMDI]").fancybox({
+		'transitionIn'		: 'fade',
+		'transitionOut'		: 'fade',
+		'titlePosition' 	: 'outside',
+		'titleFormat'       : function(title, currentArray, currentIndex, currentOpts) {
+		    return '<span id="fancybox-title-over">'+(currentIndex + 1)+' / '+currentArray.length+'&nbsp;&nbsp;&nbsp;'+title+'</span>';
+		}
+	});
+	
+	/*****************************************************************************/
+	//MM
+	/*****************************************************************************/
+	$("a[rel=HBAW]").fancybox({
+		'transitionIn'		: 'fade',
+		'transitionOut'		: 'fade',
+		'titlePosition' 	: 'outside',
+		'titleFormat'       : function(title, currentArray, currentIndex, currentOpts) {
+		    return '<span id="fancybox-title-over">'+(currentIndex + 1)+' / '+currentArray.length+'&nbsp;&nbsp;&nbsp;'+title+'</span>';
+		}
+	});
+	$("a[rel=OA]").fancybox({
+		'transitionIn'		: 'fade',
+		'transitionOut'		: 'fade',
+		'titlePosition' 	: 'outside',
+		'titleFormat'       : function(title, currentArray, currentIndex, currentOpts) {
+		    return '<span id="fancybox-title-over">'+(currentIndex + 1)+' / '+currentArray.length+'&nbsp;&nbsp;&nbsp;'+title+'</span>';
+		}
+	});
+	$("#OAswf").fancybox({
+	    'padding'           : 0,
+        'autoScale'     	: false,
+		'width'				: 720,
+		'height'			: 520,
+        'transitionIn'		: 'none',
+		'transitionOut'		: 'none'
+	});
+	$("a[rel=ET]").fancybox({
+		'transitionIn'		: 'fade',
+		'transitionOut'		: 'fade',
+		'titlePosition' 	: 'outside',
+		'titleFormat'       : function(title, currentArray, currentIndex, currentOpts) {
+		    return '<span id="fancybox-title-over">'+(currentIndex + 1)+' / '+currentArray.length+'&nbsp;&nbsp;&nbsp;'+title+'</span>';
+		}
+	});
+	$("a[rel=ET2]").fancybox({
+		'transitionIn'		: 'fade',
+		'transitionOut'		: 'fade',
+		'titlePosition' 	: 'outside',
+		'titleFormat'       : function(title, currentArray, currentIndex, currentOpts) {
+		    return '<span id="fancybox-title-over">'+(currentIndex + 1)+' / '+currentArray.length+'&nbsp;&nbsp;&nbsp;'+title+'</span>';
+		}
+	});
+	
+	/*****************************************************************************/
+	//BANNER
+	/*****************************************************************************/
+	$("a[rel=ODLACN]").fancybox({
+	    'padding'           : 0,
+		'width'				: 728,
+		'height'			: 90,
+        'transitionIn'		: 'none',
+		'transitionOut'		: 'none',
+		'titlePosition' 	: 'outside',
+		'titleFormat'       : function(title, currentArray, currentIndex, currentOpts) {
+		    return '<span id="fancybox-title-over">'+(currentIndex + 1)+' / '+currentArray.length+'&nbsp;&nbsp;&nbsp;'+title+'</span>';
+		}
+	});
+	$("a[rel=ODLVIP]").fancybox({
+		'transitionIn'		: 'fade',
+		'transitionOut'		: 'fade',
+		'titlePosition' 	: 'outside',
+		'titleFormat'       : function(title, currentArray, currentIndex, currentOpts) {
+		    return '<span id="fancybox-title-over">'+(currentIndex + 1)+' / '+currentArray.length+'&nbsp;&nbsp;&nbsp;'+title+'</span>';
+		}
+	});
+	$("#ODLTWITTER1").fancybox({
+	    'padding'           : 0,
+		'width'				: 728,
+		'height'			: 90,
+        'transitionIn'		: 'none',
+		'transitionOut'		: 'none',
+		'titlePosition' 	: 'outside',
+		'titleFormat'       : function(title) {
+		    return '<span id="fancybox-title-over">'+title+'</span>';
+		}
+	});
+	$("a[rel=ETEDM]").fancybox({
+		'transitionIn'		: 'fade',
+		'transitionOut'		: 'fade',
+		'titlePosition' 	: 'outside',
+		'titleFormat'       : function(title, currentArray, currentIndex, currentOpts) {
+		    return '<span id="fancybox-title-over">'+(currentIndex + 1)+' / '+currentArray.length+'&nbsp;&nbsp;&nbsp;'+title+'</span>';
+		}
+	});
+	$("#LDHBNR").fancybox({
+	    'padding'           : 0,
+		'width'				: 468,
+		'height'			: 60,
+        'transitionIn'		: 'none',
+		'transitionOut'		: 'none',
+		'titlePosition' 	: 'outside',
+		'titleFormat'       : function(title) {
+		    return '<span id="fancybox-title-over">'+title+'</span>';
+		}
+	});
+	$("a[rel=EA]").fancybox({
+	    'padding'           : 0,
+		'width'				: 630,
+		'height'			: 150,
+        'transitionIn'		: 'none',
+		'transitionOut'		: 'none',
+		'titlePosition' 	: 'outside',
+		'titleFormat'       : function(title, currentArray, currentIndex, currentOpts) {
+		    return '<span id="fancybox-title-over">'+(currentIndex + 1)+' / '+currentArray.length+'&nbsp;&nbsp;&nbsp;'+title+'</span>';
+		}
+	});
+	$("#MBL").fancybox({
+	    'padding'           : 0,
+		'width'				: 500,
+		'height'			: 338,
+        'transitionIn'		: 'none',
+		'transitionOut'		: 'none',
+		'titlePosition' 	: 'outside',
+		'titleFormat'       : function(title) {
+		    return '<span id="fancybox-title-over">'+title+'</span>';
+		},
+		'swf'				: {wmode: 'opaque'}
+	});
+
+	/*****************************************************************************/
+	//PS
+	/*****************************************************************************/
+	$("a[rel=DEST]").fancybox({
+		'transitionIn'		: 'fade',
+		'transitionOut'		: 'fade',
+		'titlePosition' 	: 'outside',
+		'titleFormat'       : function(title, currentArray, currentIndex, currentOpts) {
+		    return '<span id="fancybox-title-over">'+(currentIndex + 1)+' / '+currentArray.length+'&nbsp;&nbsp;&nbsp;'+title+'</span>';
+		}
+	});
+	$("#DEST3").fancybox({
+	    'padding'           : 0,
+        'autoScale'     	: false,
+		'width'				: 640,
+		'height'			: 512,
+        'transitionIn'		: 'none',
+		'transitionOut'		: 'none',
+		'type'				: 'iframe'
+	});
+	$("a[rel=VEGE]").fancybox({
+		'transitionIn'		: 'fade',
+		'transitionOut'		: 'fade',
+		'titlePosition' 	: 'outside',
+		'titleFormat'       : function(title, currentArray, currentIndex, currentOpts) {
+		    return '<span id="fancybox-title-over">'+(currentIndex + 1)+' / '+currentArray.length+'&nbsp;&nbsp;&nbsp;'+title+'</span>';
+		}
+	});
+	$("a[rel=VEGE2]").fancybox({
+		'transitionIn'		: 'fade',
+		'transitionOut'		: 'fade',
+		'titlePosition' 	: 'outside',
+		'titleFormat'       : function(title, currentArray, currentIndex, currentOpts) {
+		    return '<span id="fancybox-title-over">'+(currentIndex + 1)+' / '+currentArray.length+'&nbsp;&nbsp;&nbsp;'+title+'</span>';
+		}
+	});
+	
+});
