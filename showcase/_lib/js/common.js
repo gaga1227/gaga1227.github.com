f86d5c062463212fbfb951e2cee91e53f3c73df6
Johnny Xu <gaga1227@gmail.com>
2013-06-24 21:20:02 +1000

    added slabtext test

--- showcase/_lib/js/common.js
+++ showcase/_lib/js/common.js
@@ -2,7 +2,15 @@
 /* init */
 /* ------------------------------------------------------------------------------ */
 function init(){
-	$('.fittext').fitText();
+	//$('.fittext').fitText();
+	function slabTextHeadlines() {
+        $('.fittext').slabText({
+            // Don't slabtext the headers if the viewport is under 380px
+            "viewportBreakpoint":380
+        });
+    };
+	setTimeout(slabTextHeadlines, 1000);
+	//slabTextHeadlines();
 }
 /* DOM.ready */
 $(document).ready(function(){ 
